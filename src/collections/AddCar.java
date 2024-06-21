ad6f892f2619aaaacd0eceb725b21eb4114e4513
KiranLearning <131030328+KiranLearning@users.noreply.github.com>
2024-06-22 00:39:47 +0530

    Create AddCar.java

---
+++ src/collections/AddCar.java
@@ -0,0 +1,14 @@
+package collections;
+
+public class AddCar {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public void addCar() {
+		String name="Maruti";
+		System.out.println("Dev 2"+name);
+	}
+ 
+}
